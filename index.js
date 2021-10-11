507da03ad764f84b8518a0fda8ee0a3a217b3b6e
Alessandro Gomes <ale.gomesilva@hotmail.com>
2021-10-11 10:03:18 -0300

    add unlocoded

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   
 const { Console } = require("console");
 const express = require("express");
@@ -92,57 +91,4 @@ app.post("/subscription", (req, res) => {
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
-=======
-  
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const port = 3000;
-
-
-
-let message = "";
-let poke = [];
-
-const pokedex = [{id: 1, numero:"1",nome:"Charizard",tipo:"Incêndio",imagem:"charizard.png",descricao:"Dragão",altura:"1.70 m",peso:"90.5 kg",categoria:"Chama",habilidade:"Chame Alta" },
-{id: 2, numero:"2",nome:"Blastoise",tipo:"Água",imagem:"blastoise.png",descricao:"Tartaruga",altura:"1.60 m",peso:"85.5 Kg",categoria:"Marisco",habilidade:"Move Água" },
-{id: 3, numero:"3",nome:"Sandslash",tipo:"Chão",imagem:"sandslash.png",descricao:"Rato Espinhoso",altura:"1.00 m",peso:"29.5 Kg",categoria:"Mouse",habilidade:"Vé de Areia" }];
-
-
-
-app.use(express.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", function (req, res) {
-    res.render ("index", {pokedex: pokedex});
-});
-
-app.get("/", function (req, res) {
-  poke = {nome, imagem, tipo};
-  res.render ("cadastro",{pokedex: [{nome:nome, imagem:imagem, tipo: tipo}]})  
-});
-
-app.get ("/", (req, res) => {
-  poke = {numero, descricao, altura, peso, categoria, habilidade};
-  res.render ("/detalhe", {pokedex: [{numero: numero, descricao: descricao, altura: altura, peso: peso, categoria: categoria, habilidade: habilidade}]})
-});
-
-app.get ("/detalhe", function(req, res){
-  res.render ("cadastro")
-})
-
-app.post ("/cadastro", function (req, res){
-  res.render ("cadastro")
-})
-
-// app.post ("/cadastro", (req, res) => {
-//   const pokenovo = req.body;
-//   res.redirect ("/")
-// });
-
-app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`)
->>>>>>> 6875660908784701f31789a06825e0719d1c0d76
 );
